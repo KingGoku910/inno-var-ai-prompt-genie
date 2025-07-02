d18b59263d2e85cc851d0545edc37e4bbe8f69a6
Gerbrand Ryno Rossouw <74030806+KingGoku910@users.noreply.github.com>
2025-07-02 17:10:20 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,18 +18,18 @@ export const Header: React.FC<HeaderProps> = ({ activeSection, setActiveSection 
   return (
     <header className="bg-white dark:bg-black border-b border-gray-200 dark:border-gray-700 sticky top-0 z-40">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-25">
           {/* Logo */}
           <div className="flex items-center space-x-3">
             <div className="relative">
-              <img src="https://imgur.com/Qbn547r.png" className="w-16 h-16" />
+              <img src="https://imgur.com/Qbn547r.png" className="w-18 h-18" />
             </div>
             <div>
-              <h1 className="text-xl font-bold text-gray-900 dark:text-white">
-                 AI-<span className="text-blue-600">Prompt</span>Genie
+              <h1 className="text-xl2 font-bold text-gray-900 dark:text-white">
+                 AI-<span className="text-colorHighlight">Prompt</span>Genie
               </h1>
               <p className="text-xs text-gray-900 dark:text-gray-400">
-                 by <a href="https://innovatex-ai.net">Innova<span className="text-blue-600 font-bold">TEX</span>-AI</a>
+                 by <a href="https://innovatex-ai.net">Innova<span className="text-colorHighlight font-bold">TEX</span>-AI</a>
               </p>
             </div>
           </div>
